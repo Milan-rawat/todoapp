ddd35039ab3eafa7766eb5fc0dca5a1be2e1ad15
Milan Rawat <milanrawat086@gmail.com>
2022-06-26 19:23:58 +0530

    header

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const path = require("path");
+const fs = require("fs");
 const taskRouter = require("./routes/taskRoutes");
 
 // Start express app
@@ -9,6 +11,12 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 
 // Routes
+app.use(express.static(path.join(__dirname, "todoapp/build")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "todoapp/build", "index.html"));
+});
+
 app.use("/tasks", taskRouter);
 
 app.all("*", (req, res, next) => {
---
+++ todoapp/src/components/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+import style from "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <h2>Task Manager App</h2>
+      <div className="buttons">
+        <div className="allTasks">All Tasks</div>
+        <div className="doneTasks">Completed Tasks</div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
